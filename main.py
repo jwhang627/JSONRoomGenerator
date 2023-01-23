da9545961a97d637b3be5debc71600b5cdcc4e4d
Jay Whang <jaywhang627@gmail.com>
2023-01-23 01:28:29 -0500

    Updating.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import json, random, os, numpy, re, ast, array
 
 max_num_rooms = 10
-max_num_doors = 4
+max_num_doors = 3
 
+
+# room_0000, room_0001, etc.
 
 def generate_json_map(n_r):
     #print(n_r)
     #rooms = ["{0}".format(str(i + 1).zfill(2)) for i in range(n_r)]
-    rooms = [str(i + 1) for i in range(n_r)]
+    rooms = ["room_00%02d"%i for i in range(n_r)]
+    
     #rooms = list(rooms)
-    rooms_string = json.dumps(rooms)
+    #rooms_string = json.dumps(rooms)
     #print(json.dumps(str(rooms).replace("\'","\"")))
     #print(rooms)
     #rooms = json.dumps(rooms)
@@ -17,7 +20,7 @@ def generate_json_map(n_r):
     str_map = "{"
     for i in range(n_r):
         #str_map += (str("{0}".format(str(i+1).zfill(2))) + ":" + "{")
-        str_map += (str(i+1) + ":" + "{")
+        str_map += ("\"" + rooms[i] + "\"" + ":" + "{")
         str_map += "\"num_of_doors\":"
         num_doors = random.randint(1,max_num_doors)
         str_map += (str(num_doors) + ",")
@@ -30,8 +33,8 @@ def generate_json_map(n_r):
             while str("{0}".format(str(i + 1).zfill(2))) in portals:
                 portals = numpy.random.choice(rooms,num_doors,False)
         '''
-        if str(i + 1) in portals:
-            while str(i + 1) in portals:
+        if ("room_00%02d"%i) in portals:
+            while ("room_00%02d"%i) in portals:
                 portals = numpy.random.choice(rooms,num_doors) 
         str_map += (re.sub("\s+",",",str(portals).strip()) + ",")
         
@@ -59,7 +62,7 @@ def main():
     num_rooms = random.randint(5,max_num_rooms)
     #num_doors = random.randint(1,max_num_doors)
     str_map = generate_json_map(num_rooms)
-
+    #print(str_map)
     str_map = str_map.replace("\'","\"")
     
     #p = re.compile('(?<!\\\\)\'')
@@ -69,13 +72,16 @@ def main():
 
     #json_object = json.loads(str_map)
 
-    json_file = ast.literal_eval(json.dumps(str_map,skipkeys = True, allow_nan = True,indent = 4))
-
-
+    json_file = ast.literal_eval(json.dumps(str_map,indent = 4,sort_keys=False))
+    #json_file = json.dumps(str_map,indent = 4,sort_keys=False)
+    #j = json.loads(json_file)
+    #j = json.dumps(str(j),indent = 4,sort_keys=False)
+    #print(str(j))
     with open("result.json","w+") as outfile:
+        #outfile.write(str(j))
         outfile.write(json_file)
         #json.dump(json_object, outfile)
     #with open("result.json","r+") as infile:
-    	#print(json.load(infile))
-
+    #	j = json.load(infile)
+        
 main()
